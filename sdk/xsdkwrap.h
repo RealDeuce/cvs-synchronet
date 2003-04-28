25cca81b9f8ee88934d45f53443fe170c80ce9cd
deuce <deuce>
2003-04-28 03:13:16 +0000

    Fix bugs in last commit

--- sdk/xsdkwrap.h
+++ sdk/xsdkwrap.h
@@ -2,7 +2,7 @@
 
 /* Synchronet XSDK system-call wrappers */
 
-/* $Id: xsdkwrap.h,v 1.11 2003/04/28 03:07:01 deuce Exp $ */
+/* $Id: xsdkwrap.h,v 1.12 2003/04/28 03:13:16 deuce Exp $ */
 
 /****************************************************************************
  * @format.tab-size 4		(Plain Text/Source Code File Header)			*
@@ -140,9 +140,11 @@
 	#define O_BINARY	0		/* all files in binary mode on Unix */
 	#define O_DENYNONE  (1<<31)	/* req'd for Baja/nopen compatibility */
 
+#ifndef __QNX__
 	#define SH_DENYNO	2          // no locks
 	#define SH_DENYRW	F_WRLCK	   // exclusive lock
 	#define SH_DENYWR   F_RDLCK    // shareable lock
+#endif
 
 	#define stricmp(x,y)		strcasecmp(x,y)
 	#define strnicmp(x,y,z)		strncasecmp(x,y,z)
@@ -203,8 +205,10 @@
 	#define tell(fd)			lseek(fd,0,SEEK_CUR)
 
 #ifdef __QNX__
+	#include <share.h>
 	int		qnx_sopen(char *fn, int access, int share);
 	#define sopen(x,y,z)	qnx_sopen(x,y,z)
+	#define L_SET			SEEK_SET
 #else
 	int		sopen(char *fn, int access, int share);
 #endif
