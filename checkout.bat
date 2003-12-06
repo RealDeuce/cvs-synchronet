c4ece2aa8f9002eff4b11921c87bd50d4a266de6
rswindell <rswindell>
2003-12-06 22:52:28 +0000

    Allow .debug or .release to be specified on the command-line.

--- checkout.bat
+++ checkout.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Synchronet source code check-out for Win32
-echo $Id: checkout.bat,v 1.2 2003/09/19 06:09:33 rswindell Exp $
+echo $Id: checkout.bat,v 1.3 2003/12/06 22:52:28 rswindell Exp $
 setlocal
 set HOME=c:\
 set CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs
 cvs co src-sbbs3
-cvs co lib-win32 
+cvs co lib-win32%1
