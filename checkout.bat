e6591e2b8f322720f40723f0d0294b07dc37ab25
rswindell <rswindell>
2004-11-19 23:01:51 +0000

    Check-out both debug and release libraries.

--- checkout.bat
+++ checkout.bat
@@ -1,8 +1,9 @@
 @echo off
 echo Synchronet source code check-out for Win32
-echo $Id: checkout.bat,v 1.3 2003/12/06 22:52:28 rswindell Exp $
+echo $Id: checkout.bat,v 1.4 2004/11/19 23:01:51 rswindell Exp $
 setlocal
 set HOME=c:\
 set CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs
 cvs co src-sbbs3
-cvs co lib-win32%1
+cvs co lib-win32.debug
+cvs co lib-win32.release
