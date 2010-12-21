444bcdb7421c92a89ee1111664b9db7a8c30dc70
mcmlxxix <mcmlxxix>
2010-12-21 21:56:38 +0000

    chat now uses IRC link, misc updates

--- synchronetris/tetris.js
+++ synchronetris/tetris.js
@@ -3,7 +3,7 @@
 	For Synchronet v3.15+
 	Matt Johnson(2009)
 */
-//$Id: tetris.js,v 1.8 2010/10/01 14:33:04 mcmlxxix Exp $
+//$Id: tetris.js,v 1.9 2010/12/21 21:56:38 mcmlxxix Exp $
 load("commclient.js");
 
 var root = js.exec_dir;
@@ -12,9 +12,8 @@ var stream = new ServiceConnection("tetris");
 var server_file=new File(root + "server.ini");
 if(file_exists(server_file.name)) {
 	server_file.open('r',true);
-	var update=server_file.iniGetValue("autoupdate");
+	var update=server_file.iniGetValue(null,"autoupdate");
 	server_file.close();
-	
 	if(update != true) {
 		if((user.compare_ars("SYSOP") || (bbs.sys_status&SS_TMPSYSOP)) 
 			&& console.yesno("Check for game updates?")) 
