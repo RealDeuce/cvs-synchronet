8e82825e7b468ed36a2419a6de951b6da58324ca
rswindell <rswindell>
2011-10-27 01:23:59 +0000

    Fixed the lib-win32 module alias.

--- checkout.bat
+++ checkout.bat
@@ -1,9 +1,8 @@
 @echo off
 echo Synchronet source code check-out for Win32
-echo $Id: checkout.bat,v 1.4 2004/11/19 23:01:51 rswindell Exp $
+echo $Id: checkout.bat,v 1.5 2011/10/27 01:23:59 rswindell Exp $
 setlocal
 set HOME=c:\
 set CVSROOT=:pserver:anonymous@cvs.synchro.net:/cvsroot/sbbs
 cvs co src-sbbs3
-cvs co lib-win32.debug
-cvs co lib-win32.release
+cvs co lib-win32
