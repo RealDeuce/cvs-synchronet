51a9fb3333c7a3a9d911b6ce0988ac63ec09512b
Stephen Hurd <deuce@synchro.net>
2020-04-26 02:18:18 -0400

    Merge cvs-synchronet-exec as exec

---
+++ exec/ftn-setup.js
@@ -0,0 +1,120 @@
+// $Id: ftn-setup.js,v 1.10 2020/04/25 09:22:01 rswindell Exp $
+
+load('sbbsdefs.js');
+load('frame.js');
+load('tree.js');
+const fidoaddr = load({}, 'fidoaddr.js');
+
+js.on_exit('console.attributes = ' + console.attributes);
+js.on_exit('bbs.sys_status = ' + bbs.sys_status);
+
+bbs.sys_status|=SS_MOFF;
+
+const addrs = {};
+system.fido_addr_list.forEach(function (e) {
+    const a = fidoaddr.parse(e);
+    addrs[a.zone] = e;
+});
+
+const frame = new Frame(1, 1, console.screen_columns, console.screen_rows, BG_BLUE|WHITE);
+const main_frame = new Frame(1, 2, frame.width, frame.height - 2, BG_BLACK|LIGHTGRAY, frame);
+const tree_frame = new Frame(1, main_frame.y + 1, Math.floor(main_frame.width / 2), main_frame.height - 2, BG_BLACK|LIGHTGRAY, main_frame);
+const info_frame = new Frame(tree_frame.width + 1, main_frame.y + 1, main_frame.width - tree_frame.width - 1, main_frame.height - 2, BG_BLACK|WHITE, main_frame);
+info_frame.word_wrap = true;
+const tree = new Tree(tree_frame);
+
+frame.putmsg('FTN Setup');
+frame.gotoxy(1, frame.height);
+frame.putmsg('[Up/Down/Home/End] to navigate, [Enter] to select, [Q] to quit');
+
+tree.colors.fg = LIGHTGRAY;
+tree.colors.bg = BG_BLACK;
+tree.colors.lfg = WHITE;
+tree.colors.lbg = BG_CYAN;
+tree.colors.kfg = LIGHTCYAN;
+
+var longest = 0;
+var f = new File(system.exec_dir + 'init-fidonet.ini');
+if (f.open('r')) {
+    f.iniGetSections('zone:', 'zone').forEach(function (e) {
+        const net = f.iniGetObject(e);
+        const zone = e.substr(5);
+        const item = tree.addItem(format('Zone %5d: %s', zone, net.name), function () {
+            console.clear(BG_BLACK|LIGHTGRAY);
+            js.exec('init-fidonet.js', {}, zone);
+            frame.invalidate();
+        });
+        if (item.text.length > longest) longest = item.text.length;
+        item.__ftn_setup = net;
+        item.__ftn_setup._zone_number = parseInt(zone, 10);
+    });
+	f.close();
+}
+
+tree_frame.width = longest + 1;
+info_frame.x = tree_frame.width + 2;
+info_frame.width = main_frame.width - tree_frame.width - 2;
+
+console.clear(BG_BLACK|LIGHTGRAY);
+frame.open();
+tree.open();
+frame.cycle();
+
+var key;
+var zone;
+var high_water = 0;
+console.ungetstr(KEY_UP);
+while (!js.terminated) {
+    key = console.getkey();
+    if (key.toLowerCase() == 'q') break;
+    tree.getcmd(key);
+    if (key == KEY_UP || key == KEY_DOWN || key == KEY_HOME || key == KEY_END) {
+        zone = tree.currentItem.__ftn_setup;
+	function crlf() {
+		info_frame.cleartoeol();
+		info_frame.crlf();
+	}
+        info_frame.home();
+        info_frame.putmsg('\1h\1w' + zone.name);
+	crlf();
+        if (zone.desc) {
+		info_frame.putmsg('\1n\1w' + zone.desc);
+		crlf();
+		crlf();
+	}
+        if (zone.info) {
+		info_frame.putmsg('\1h\1cInformation\1w:');
+		crlf();
+		info_frame.putmsg('\1n' + zone.info);
+		crlf();
+		crlf();
+	}
+        if (zone.coord) {
+		info_frame.putmsg('\1h\1cCoordinator\1w:');
+		crlf();
+		info_frame.putmsg('\1n' + zone.coord);
+		crlf();
+		crlf();
+	}
+        if (zone.email) {
+		info_frame.putmsg('\1h\1cEmail\1w:');
+		crlf();
+		info_frame.putmsg('\1n' + zone.email);
+		crlf();
+		crlf();
+	}
+        if (addrs[zone._zone_number]) {
+		info_frame.putmsg('\1h\1rExisting address found: ' + addrs[zone._zone_number]);
+		crlf();
+        }
+	var y = info_frame.cursor.y;
+	while(high_water > y) {
+		crlf();
+		high_water--;
+	}
+	high_water = y;
+    }
+    if (frame.cycle()) console.gotoxy(console.screen_columns, console.screen_rows);
+}
+
+frame.close();
